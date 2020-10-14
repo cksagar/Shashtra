ba02f30b2cd5fb929c482bd039ac74acb464ec0d
chetan <cksagar7@gmail.com>
2020-10-14 23:16:32 +0530

    small chnges

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,5 +1,5 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -10,7 +10,6 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(map(result => result.matches),
       shareReplay()
@@ -21,6 +20,5 @@ export class MainNavComponent {
 
   logout() {
     this.router.navigate(['/login']);
-
   }
 }
